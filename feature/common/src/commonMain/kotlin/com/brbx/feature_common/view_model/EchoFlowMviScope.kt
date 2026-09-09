fbc2ec737414237298b0fe546cf1017e4782b09f
BRBX <egorka2005sautkin@gmail.com>
2026-09-09 13:13:30 +0300

    fix bug with paging

--- feature/common/src/commonMain/kotlin/com/brbx/feature_common/view_model/EchoFlowMviScope.kt
+++ feature/common/src/commonMain/kotlin/com/brbx/feature_common/view_model/EchoFlowMviScope.kt
@@ -2,5 +2,5 @@ package com.brbx.feature_common.view_model
 
 import com.brbx.mvi_core.contracts.MviScope
 
-interface EchoFlowMviScope<State, in Intent : Any, ScreenEffect : Any> :
+typealias EchoFlowMviScope<State, Intent, ScreenEffect> =
     MviScope<State, EchoFlowEffect, ScreenEffect, Intent>
--- feature/common/src/commonMain/kotlin/com/brbx/feature_common/view_model/EchoFlowViewModel.kt
+++ feature/common/src/commonMain/kotlin/com/brbx/feature_common/view_model/EchoFlowViewModel.kt
@@ -1,36 +1,10 @@
 package com.brbx.feature_common.view_model
 
-import androidx.lifecycle.viewModelScope
 import com.brbx.mvi_core.base.ContainedMviViewModel
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.flow.SharedFlow
-import kotlinx.coroutines.flow.StateFlow
 import org.koin.core.component.KoinComponent
-import org.koin.core.component.inject
-import org.koin.core.parameter.parametersOf
 
 abstract class EchoFlowViewModel<State, in Intent : Any, ScreenEffect : Any>(
     initialState: State,
     effectReplay: Int = 1,
 ) : ContainedMviViewModel<State, EchoFlowEffect, ScreenEffect, Intent>(initialState, effectReplay),
-    KoinComponent {
-
-    override val scope: EchoFlowMviScope<State, Intent, ScreenEffect> = object : EchoFlowMviScope<State, Intent, ScreenEffect> {
-        override val viewModelScope: CoroutineScope = this@EchoFlowViewModel.viewModelScope
-        override val state: StateFlow<State> = this@EchoFlowViewModel.state
-        override val effects: SharedFlow<EchoFlowEffect> = this@EchoFlowViewModel.effects
-        override val screenEffects: SharedFlow<ScreenEffect> = this@EchoFlowViewModel.screenEffects
-
-        override fun reduce(reducer: State.() -> State) =
-            this@EchoFlowViewModel.reduce(reducer)
-
-        override fun dispatchIntent(intent: Intent) =
-            this@EchoFlowViewModel.dispatchIntent(intent)
-
-        override fun postEffect(effect: EchoFlowEffect) =
-            this@EchoFlowViewModel.postEffect(effect)
-
-        override fun postScreenEffect(effect: ScreenEffect) =
-            this@EchoFlowViewModel.postScreenEffect(effect)
-    }
-}
+    KoinComponent
