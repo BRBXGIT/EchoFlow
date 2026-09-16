849e47384a068e4ced9d5fadeab529b0e9c29e1a
BRBX <egorka2005sautkin@gmail.com>
2026-09-16 22:02:44 +0300

    Change seed color

--- core/design-system/theme/src/commonMain/kotlin/com/brbx/design_system/theme/Color.kt
+++ core/design-system/theme/src/commonMain/kotlin/com/brbx/design_system/theme/Color.kt
@@ -2,4 +2,4 @@ package com.brbx.design_system.theme
 
 import androidx.compose.ui.graphics.Color
 
-internal val EchoFlowSeedColor = Color(0xff6a4a2c)
+internal val EchoFlowSeedColor = Color(0xffd0bcff)
